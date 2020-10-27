e47e457042a63a45400f899829144ae889244912
Dong-Young Kim <31337.persona@gmail.com>
2020-10-28 01:17:49 +0900

    feat(auth): add auth page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
+import Home from "../views/Home.vue";
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +11,10 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/signin",
+    alias: "/signup",
+    name: "Auth",
+    component: () => import(/* webpackChunkName: "auth" */ "../views/Auth.vue")
   }
 ];
 
