9682b94f406586280de1ab4ab0a490aa0ed0c8c6
Dong-Young Kim <31337.persona@gmail.com>
2020-10-28 13:19:47 +0900

    feat(auth): add auth components

---
+++ src/components/Auth/SignIn.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Sign in
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="email"
+          label="Email"
+          prepend-icon="mdi-email"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="isPasswordHide ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="isPasswordHide ? 'password' : 'text'"
+          @click:append="isPasswordHide = !isPasswordHide"
+        ></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text color="primary">Submit</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "SignIn",
+  data: () => ({
+    email: "",
+    password: "",
+    isPasswordHide: true
+  })
+};
+</script>
---
+++ src/components/Auth/SignUp.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Sign up
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="email"
+          label="Email"
+          prepend-icon="mdi-email"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="isPasswordHide ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="isPasswordHide ? 'password' : 'text'"
+          @click:append="isPasswordHide = !isPasswordHide"
+        ></v-text-field>
+        <v-text-field
+          v-model="passwordConfirm"
+          label="Confirm Password"
+          prepend-icon="mdi-lock"
+          type="password"
+        ></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text color="primary">Submit</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  data: () => ({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    isPasswordHide: true
+  })
+};
+</script>
