a39eea9fb53c19b6f5f4033e7ba75e41ece64e19
Dong-Young Kim <31337.persona@gmail.com>
2020-10-28 17:02:23 +0900

    feat: add axios plugin

---
+++ src/plugins/axios.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import axios from "axios";
+
+Plugin.install = (Vue, options) => {
+  const defaultOptions = {
+    instance: {
+      baseURL: "http://localhost:3000",
+      timeout: 1000 * 10
+    },
+    interceptor: {
+      request: {
+        handle: config => config,
+        error: error => Promise.reject(error)
+      },
+      response: {
+        handle: response => response,
+        error: error => Promise.reject(error)
+      }
+    }
+  };
+
+  const { instance, interceptor } = {
+    ...defaultOptions,
+    ...options
+  };
+
+  const _axios = axios.create(instance);
+
+  _axios.interceptors.request.use(
+    config => interceptor.request.handle(config),
+    error => interceptor.request.error(error)
+  );
+
+  _axios.interceptors.response.use(
+    response => interceptor.response.handle(response),
+    error => interceptor.response.error(error)
+  );
+
+  Vue.axios = _axios;
+  Vue.prototype.$axios = _axios;
+};
+
+Vue.use(Plugin);
+
+export default Plugin;
