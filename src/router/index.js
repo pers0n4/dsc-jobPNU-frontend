053598138bf11eb9843322e77691cb1aefe51552
Dong-Young Kim <31337.persona@gmail.com>
2020-11-09 20:51:54 +0900

    fix(router): fix export

--- src/router/index.js
+++ src/router/index.js
@@ -11,43 +11,45 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/signin",
     alias: "/signup",
     name: "Auth",
-    component: () => import(/* webpackChunkName: "auth" */ "../views/Auth.vue")
+    component: () => import(/* webpackChunkName: "auth" */ "../views/Auth.vue"),
   },
   {
     path: "/mypage",
     name: "Mypage",
-    component: () => import("../views/MyPage.vue")
+    component: () => import("../views/MyPage.vue"),
   },
   {
     path: "/",
     name: "Board",
-    component: Board
+    component: Board,
   },
   {
     path: "/",
     name: "Read",
-    component: Read
+    component: Read,
   },
   {
     path: "/create/:contentId?",
     name: "Create",
-    component: Create
+    component: Create,
   },
   {
     path: "/detail/:contentId",
     name: "Detail",
-    component: () => import("@/components/Detail")
-  }
+    component: () => import("@/components/Detail"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
+
+export default router;
