7111d5d32dfc8955536dc5890b7423b77a8774fb
ji-one <ds020228@naver.com>
2020-11-13 01:18:05 +0900

    ui change (creat, detail, board)

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>스터디 그룹 모집 목록</h1>
+    <h2 class="text-h2">Search Study ✏️</h2>
     <v-container grid-list-md>
       <v-data-table :headers="headers" :items="data" @click:row="detail">
       </v-data-table>
       <v-row>
-        <v-btn outlined color="blue" @click="write">스터디 생성</v-btn>
+        <!--<v-btn outlined color="blue" @click="write">스터디 생성</v-btn>-->
       </v-row>
     </v-container>
   </div>
@@ -20,12 +20,12 @@ export default {
     return {
       headers: [
         {
-          text: "제목",
+          text: "Title",
           value: "title",
           sortable: true
         },
         {
-          text: "작성자",
+          text: "Name",
           value: "user",
           sortable: false
         }
@@ -42,11 +42,6 @@ export default {
         path: "create"
       });
     },
-
-    // detail(item) {
-    //     this.$router.push('/detail/' + item.id)
-    // }
-
     detail(item) {
       this.$router.push({
         name: "Detail",
